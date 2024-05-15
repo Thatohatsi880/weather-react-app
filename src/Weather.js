b26d947901af39ed23f6e332d1e6ebc4406fdaa0
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-05-15 16:33:25 +0200

    imported axios into the Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 
 export default function Weather(){
-    return<h3> After a very long time coming!!</h3>
+
+    function handleResponse(response){
+        alert(`The weather in Springs is ${response.data.main.temp}`);
+    }
+    let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/Weather?q=Springs&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(handleResponse);
+    return<h3> After a very long time coming!!</h3>;
 }
