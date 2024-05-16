3fc55a1368d6dd30658149f4e19359b5be8f8882
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-05-16 08:21:11 +0200

    changed a fault on my api url

--- src/Weather.js
+++ src/Weather.js
@@ -21,7 +21,7 @@ export default function Weather(props){
         alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
     }
     let apiKey = "3a94f3778290bfeee61278505dbbe51d";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/Weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
     
     axios.get(apiUrl).then(handleResponse);
     return(
